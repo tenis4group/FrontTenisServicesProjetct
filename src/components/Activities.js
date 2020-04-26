0406f86ad86061fe9b01333fbb95f957f929b013
Artur Michalak <artur93michalak@wp.pl>
2020-04-26 14:13:15 +0200

    Add activities

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,7 +1,28 @@
 import React from "react";
+import "../styles/Activities.css";
+
+import activities1 from "../images/activities/dzieci.jpg";
+import activities2 from "../images/activities/przedszkolaki.jpg";
+import activities3 from "../images/activities/instruktor.jpg";
+import activities4 from "../images/activities/seniorzy.jpg";
 
 const Activities = () => {
-  return <div className="activities">Activities</div>;
+  return (
+    <div className="activities">
+      <div>
+        <img src={activities1} alt="zajecia1" />
+      </div>
+      <div>
+        <img src={activities2} alt="zajecia2" />
+      </div>
+      <div>
+        <img src={activities3} alt="zajecia3" />
+      </div>
+      <div>
+        <img src={activities4} alt="zajecia4" />
+      </div>
+    </div>
+  );
 };
 
 export default Activities;
