9796800aa39549f172a247f99da1c4cfe12046b8
MagdaDajewska <62498721+MagdaDajewska@users.noreply.github.com>
2020-04-26 23:08:58 +0200

    Add files via upload

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import Form from  'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import RegistrationAlert from './RegistrationAlert';
-import "../../styles/Registration.css";
 
 
 class Registration extends Component {
