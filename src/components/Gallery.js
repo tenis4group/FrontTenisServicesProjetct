ff5f56dee52d26f57035337502953497f48146ca
MagdaDajewska <62498721+MagdaDajewska@users.noreply.github.com>
2020-04-26 22:43:34 +0200

    Add files via upload

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,7 +1,45 @@
 import React from "react";
+import Carousel from 'react-bootstrap/Carousel';
 
-const Gallery = () => {
-  return <div className="gallery">Gallery</div>;
-};
+ function Gallery () {
+   return(
+    <div className="Gallery">
+  <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://hotelwieniawa.com/wp-content/uploads/2018/08/korty-tenisowe-reda-puck-wejherowo-large-e1421681171678.jpg"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>Pierwszy slajd</h3>
+    </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://hotelwieniawa.com/wp-content/uploads/2018/08/korty-tenisowe-reda-puck-wejherowo-large-e1421681171678.jpg"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Drugi slajd</h3>
+    </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://hotelwieniawa.com/wp-content/uploads/2018/08/korty-tenisowe-reda-puck-wejherowo-large-e1421681171678.jpg"
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Trzeci slajd</h3>
+    </Carousel.Caption>
+   </Carousel.Item>
+    </Carousel>
+   </div>
+   )
+  };
 
 export default Gallery;
